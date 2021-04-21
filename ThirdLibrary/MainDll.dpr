d9634e0f9379efa3d5ac75e2b4edaa9538944ecb
bunn1k <DISKO312@mail.ru>
2021-04-21 11:41:29 +0700

    Задание 12.3.1 Подключение динамически библиотеки MainDll.dll

---
+++ ThirdLibrary/MainDll.dpr
@@ -0,0 +1,36 @@
+library MainDll;
+
+
+uses
+  System.SysUtils,
+  System.Classes, ShareMem;
+
+{$R *.res}
+
+function SummaX_Y(X,Y:real):real; stdcall; {функция возращающая сумму двух чисел X и Y}
+begin
+  result:=X+Y;
+end;
+
+function ProizvX_Y(X,Y:real):real; stdcall; {функция возвращающая произведение двух чисел X и Y}
+begin
+  result:=X*Y;
+end;
+
+function Revers(S:string):string; stdcall; {Функция возвращающая строку S задом-наперед}
+var i,L:byte; S_:string;
+begin
+  L:=length(S);
+  S_:='';
+  for i := 1 to L do
+    S_:=S_+S[L-i+1];
+  result:=S_;
+end;
+
+exports
+  SummaX_Y,
+  ProizvX_Y,
+  Revers;
+
+begin
+end.
